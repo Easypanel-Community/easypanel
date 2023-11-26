69444a237ac238a39f6c3600479297ec09ee7891
productdevbook <hi@productdevbook.com>
2023-11-26 11:58:15 +0300

    feat: getUser, getLicensePayload, activateLicense

--- src/client.ts
+++ src/client.ts
@@ -1,8 +1,7 @@
 import { ofetch } from 'ofetch'
-import type { ClientConfig, ClientResponse } from './typesNew'
-import { projectsManager, servicesManager } from './managers'
-import { monitorManager } from './managers/MonitorManager'
-import { settingsManager } from './managers/SettingsManager'
+import type { ClientConfig, ClientResponse, NoResponse, UserRes } from './types'
+import { monitorManager, projectsManager, servicesManager, settingsManager } from './managers'
+import { Routes } from './utils/Routes'
 
 let token = ''
 
@@ -47,17 +46,37 @@ function client(config: ClientConfig): ClientResponse {
 }
 
 export function easypanel(config: ClientConfig) {
-  const resolvedClient = client(config)
+  const _client = client(config)
 
-  const projects = projectsManager(resolvedClient)
-  const services = servicesManager(resolvedClient)
-  const monitor = monitorManager(resolvedClient)
-  const settings = settingsManager(resolvedClient)
+  const projects = projectsManager(_client)
+  const services = servicesManager(_client)
+  const monitor = monitorManager(_client)
+  const settings = settingsManager(_client)
+
+  async function getUser(): Promise<UserRes> {
+    const res = await _client.get<UserRes>(Routes.Auth.GetUser, { json: null })
+    return res
+  }
+
+  async function getLicensePayload(type: 'lemon' | 'portal'): Promise<NoResponse> {
+    const res = await _client.get<NoResponse>(Routes.License(type).Get, { json: null })
+    return res
+  }
+
+  async function activateLicense(type: 'lemon' | 'portal'): Promise<NoResponse> {
+    const res = await _client.post<NoResponse>(Routes.License(type).Activate, {
+      json: null,
+    })
+    return res
+  }
 
   return {
     projects,
     services,
     monitor,
     settings,
+    getUser,
+    getLicensePayload,
+    activateLicense,
   }
 }
